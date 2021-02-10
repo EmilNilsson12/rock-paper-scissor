d9ebf148bc9025e7dc7327ac09719138a530415f
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-02-10 10:13:09 +0100

    Move gamefield to index.html, make more global gids in script.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gameBoard = document.getElementById('gameBoard');
 const startBtn = document.getElementById('startBtn');
+const computerHandDiv = document.getElementById('computerHand');
+const playerHandDiv = document.getElementById('playerHand');
+const gameField = document.getElementById('gameField');
 
 startBtn.addEventListener('click', startNewGame);
 
@@ -26,30 +29,104 @@ function startNewGame() {
 
 
 function renderGamePieces() {
-    const gameField = `
-        <div id="opponentHand" class="hand"></div>
-        <div id="gameBtns">
-            <button>Rock</button>
-            <button>Paper</button>
-            <button>Scissors</button>
-        </div>
-        <div id="playerHand" class="hand"></div>
+    const gameBtns = `
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
     `;
 
-    gameBoard.innerHTML = gameField;
-    
-    const gameBtns = document.getElementById('gameBtns');
-    gameBtns.addEventListener('click', playRound)
+    gameField.innerHTML = gameBtns;
+    gameField.addEventListener('click', playRound)
 }
 
 function playRound(evt) {
     // Generate computers hand
-    
+    const computerHand = getComputerHand();
+
     // Find player hand by evt.target
+    let playerHand = evt.target.innerText;
+    playerHand = getPlayerHand(playerHand);
+
+    // Compare computerhand with playerhand
+    declareWinner(playerHand, computerHand);
 
     // Display round winner
 
     // Update score
+}
+
+function getComputerHand() {
+    const computerHand = Math.floor(Math.random() * (3) + 1);
+    console.log(computerHand);
+    const computerHandDiv = document.getElementById('computerHand');
+
+    switch (computerHand) {
+        case 1:
+            computerHandDiv.innerText = 'Rock';
+            break;
+
+        case 2:
+            computerHandDiv.innerText = 'Paper';
+            break;
+
+        case 3:
+            computerHandDiv.innerText = 'Scissors';
+            break;
+
+        default:
+            break;
+    }
+    return computerHand
+}
+
+function getPlayerHand(playerHand) {
+    const playerHandDiv = document.getElementById('playerHand');
+
+    switch (playerHand) {
+        case 'Rock':
+            playerHandDiv.innerText = 'Rock';
+            return 1
+            break;
+
+        case 'Paper':
+            playerHandDiv.innerText = 'Paper';
+            return 2
+            break;
+
+        case 'Scissors':
+            playerHandDiv.innerText = 'Scissors';
+            return 3
+            break;
+
+        default:
+            break;
+    }
+}
+
+function declareWinner(player, computer) {
+    // 1: Rock
+    // 2: Paper
+    // 3: Scissors
+
+    switch (true) {
+        case (player === computer):
+            // It's a tie
+            console.log("It's a tie");
+            break;
+
+        case (computer > player && player != 1):
+            // Computer wins
+            console.log("Computer wins");
+            break;
+
+        case (player > computer && player != 1):
+            // Player wins
+            console.log("Player wins");
+            break;
+
+        default:
+            break;
+    }
 }
 
 
@@ -70,7 +147,7 @@ function updateScoreBoard() {
         console.log(player.score);
         const currentScore = player.score;
 
-        
+
     }
 }
 */
